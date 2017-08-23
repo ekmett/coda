c0ecdbef47d01184e78fd0f8b28b57280d212621
Edward Kmett <ekmett@gmail.com>
2017-08-23 16:53:09 +0200

    try caching a little earlier

--- bin/appveyor.ps1
+++ bin/appveyor.ps1
@@ -7,9 +7,9 @@ Invoke-WebRequest 'http://www.stackage.org/stack/windows-i386' -OutFile 'stack.z
 7z x -y stack.zip stack.exe
 "> stack path"
 cmd /c '.\stack path 2>&1'
+$env:APPVEYOR_SAVE_CACHE_ON_ERROR = "true"
 "> stack setup"
 cmd /c '.\stack setup 2>&1 1>&2 > nul'
-$env:APPVEYOR_SAVE_CACHE_ON_ERROR = "true"
 "> stack build"
 cmd /c 'echo | .\stack --no-terminal build --test --bench --ghc-options=-rtsopts'
 "> stack install"
