defb667113ab8d163b2e77ce98cce305c1fa0386
Edward Kmett <ekmett@gmail.com>
2017-08-23 16:44:46 +0200

    add some better progress markers

--- bin/appveyor.ps1
+++ bin/appveyor.ps1
@@ -1,10 +1,16 @@
 $ErrorActionPreference = "Stop"
 $env:PATH += ";$PWD"
 $env:STACK_ROOT = 'c:\\sr'
+"> download stack"
 Invoke-WebRequest 'http://www.stackage.org/stack/windows-i386' -OutFile 'stack.zip'
+"> unzip stack"
 7z x -y stack.zip stack.exe
+"> stack path"
 cmd /c '.\stack path 2>&1'
-cmd /c '.\stack setup 1>&2 2>&1 > nul'
+"> stack setup"
+cmd /c '.\stack setup 2>&1'
 $env:APPVEYOR_SAVE_CACHE_ON_ERROR = "true"
+"> stack build"
 cmd /c 'echo | .\stack --no-terminal build --test --bench --ghc-options=-rtsopts'
+"> stack install"
 cmd /c '.\stack install'
