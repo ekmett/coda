36899b8486fb97593d6f2422362ae7b49b2eee4a
Edward Kmett <ekmett@gmail.com>
2017-08-24 18:51:29 +0200

    pull out travis build steps into an inline shell script

---
+++ bin/travis_cabal.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+shell_session_update() {
+  echo "invoked shell_session_update"
+}
+
+coda_before_install() {
+  set -x
+  unset CC
+  if [ $TRAVIS_OS_NAME == "linux" ]; then
+    export CXX="g++-4.9" CC="gcc-4.9" DISPLAY=:99.0;
+    sh -e /etc/init.d/xvfb start;
+    sleep 3;
+  fi
+  export HAPPYVER=1.19.5
+  export ALEXVER=3.1.7
+  export PATH=~/.cabal/bin:/opt/ghc/$GHCVER/bin:/opt/cabal/$CABALVER/bin:/opt/happy/$HAPPYVER/bin:/opt/alex/$ALEXVER/bin:$PATH
+  export CABALFLAGS="$CABALFLAGS -v2"
+  set +x
+}
+
+coda_install() {
+  set -x
+  cabal --version
+  echo "$(ghc --version) [$(ghc --print-project-git-commit-id 2> /dev/null || echo '?')]"
+  travis_retry cabal update
+  "sed -i  's/^jobs:.*$/jobs: 2/' $HOME/.cabal/config"
+  time cabal install --enable-tests --enable-benchmarks --only-dependencies
+  set +x
+}
+
+coda_script() {
+  set -x
+  time cabal configure --enable-tests --enable-benchmarks
+  time cabal build
+  time cabal test --show-details=always
+  time cabal sdist
+  unzip -v dist/build/*.vsix
+  set +x
+}
+
+coda_trap() {
+  set -e
+  $*
+  set +e
+}
