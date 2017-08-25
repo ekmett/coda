9ef91fb41e3fade0392b329ad80cafc01db3cc37
Edward Kmett <ekmett@gmail.com>
2017-08-25 18:54:18 +0200

    add decidable sinks

---
+++ src/Coda/Rpc/Sink.hs
@@ -0,0 +1,21 @@
+module Coda.Rpc.Sink 
+  ( Sink(..)
+  ) where
+
+import Data.Functor.Contravariant
+import Data.Functor.Contravariant.Divisible
+import Data.Void
+
+newtype Sink a = Sink { (<~) :: a -> IO () }
+
+instance Contravariant Sink where
+  contramap f (Sink g) = Sink (g . f)
+
+instance Divisible Sink where
+  divide f (Sink g) (Sink h) = Sink $ \a -> case f a of
+    (l, r) -> g l *> h r
+  conquer = Sink $ const $ pure ()
+
+instance Decidable Sink where
+  lose f = Sink (absurd . f)   
+  choose f (Sink g) (Sink h) = Sink (either g h . f)
