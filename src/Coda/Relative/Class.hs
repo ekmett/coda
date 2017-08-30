651a31acf0c5fd605a43da5b537b6b8eb99a092d
Edward Kmett <ekmett@gmail.com>
2017-08-29 23:16:58 -0400

    Drop Relative1 and Relative2

--- src/Coda/Relative/Class.hs
+++ src/Coda/Relative/Class.hs
@@ -17,12 +17,10 @@
 
 module Coda.Relative.Class
   ( Relative(..)
-  , Relative1(..)
-  , Relative2(..)
-  , frel1
-  , frel2
-  , GRelative(..)
-  , GRelative1(..)
+  , GRelative
+  , grel
+  , frel
+  , birel
   ) where
 
 import Coda.Relative.Delta
@@ -49,7 +47,7 @@ import GHC.Generics
 class Relative a where
   rel :: Delta -> a -> a
   default rel :: (Generic a, GRelative (Rep a)) => Delta -> a -> a
-  rel d = to . grel d . from
+  rel = grel
 
 instance Relative Delta where
   rel = (<>)
@@ -59,88 +57,45 @@ instance Relative a => Relative (Maybe a) where
   rel _ Nothing = Nothing
   {-# inline rel #-}
 
-instance Relative a => Relative [a]
-instance Relative a => Relative (NonEmpty a)
-instance Relative a => Relative (Array a) where
-  rel = frel1 rel
+instance (Relative a, Relative b) => Relative (a, b) where rel = birel
+instance (Relative a, Relative b) => Relative (Either a b) where rel = birel
+instance Relative a => Relative [a] where rel = frel
+instance Relative a => Relative (NonEmpty a) where rel = frel
+instance Relative a => Relative (Array a) where rel = frel
   
 class GRelative f where
-  grel :: Delta -> f a -> f a
+  grel' :: Delta -> f a -> f a
 
 instance GRelative U1 where
-  grel _ U1 = U1
+  grel' _ U1 = U1
 
 instance GRelative V1 where
-  grel _ x = case x of {}
+  grel' _ x = case x of {}
 
 instance (GRelative f, GRelative g) => GRelative (f :*: g) where
-  grel d (f :*: g) = grel d f :*: grel d g
+  grel' d (f :*: g) = grel' d f :*: grel' d g
   
 instance (GRelative f, GRelative g) => GRelative (f :+: g) where
-  grel d (L1 f) = L1 (grel d f)
-  grel d (R1 f) = R1 (grel d f)
+  grel' d (L1 f) = L1 (grel' d f)
+  grel' d (R1 f) = R1 (grel' d f)
 
 instance Relative c => GRelative (K1 i c) where
-  grel = coerce (rel @c)
+  grel' = coerce (rel @c)
 
 instance GRelative f => GRelative (M1 i c f) where
-  grel d = M1 #. grel d .# unM1
+  grel' d = M1 #. grel' d .# unM1
 
-instance (Relative1 f, GRelative g) => GRelative (f :.: g) where
-  grel d = Comp1 #. rel1 grel d .# unComp1
+instance (Functor f, GRelative g) => GRelative (f :.: g) where
+  grel' d = Comp1 #. fmap fmap grel' d .# unComp1
 
-class Relative1 f where
-  rel1 :: (Delta -> a -> a) -> Delta -> f a -> f a
-  default rel1 :: (Generic1 f, GRelative1 (Rep1 f)) => (Delta -> a -> a) -> Delta -> f a -> f a
-  rel1 k d = to1 . grel1 k d . from1
+grel :: (Generic a, GRelative (Rep a)) => Delta -> a -> a
+grel d = to . grel' d . from
+{-# INLINE grel #-}
 
-frel1 :: Functor f => (Delta -> a -> a) -> Delta -> f a -> f a
-frel1 = fmap fmap
+frel :: (Functor f, Relative a) => Delta -> f a -> f a
+frel = fmap fmap rel
+{-# INLINE frel #-}
 
-instance Relative1 []
--- instance Relative1 Maybe
--- instance Relative a => Relative1 ((,) a)
--- instance Relative a => Relative1 (Either a)
-  
-class GRelative1 f where
-  grel1 :: (Delta -> a -> a) -> Delta -> f a -> f a
+birel :: (Bifunctor f, Relative a, Relative b) => Delta -> f a b -> f a b
+birel d = bimap (rel d) (rel d)
 
-instance GRelative1 U1 where
-  grel1 _ _ U1 = U1
-
-instance GRelative1 V1 where
-  grel1 _ _ x = case x of {}
-
-instance (GRelative1 f, GRelative1 g) => GRelative1 (f :*: g) where
-  grel1 k d (f :*: g) = grel1 k d f :*: grel1 k d g
-  
-instance (GRelative1 f, GRelative1 g) => GRelative1 (f :+: g) where
-  grel1 k d (L1 f) = L1 (grel1 k d f)
-  grel1 k d (R1 g) = R1 (grel1 k d g)
-
-instance Relative c => GRelative1 (K1 i c) where
-  grel1 _ = coerce (rel @c)
-
-instance GRelative1 f => GRelative1 (M1 i c f) where
-  grel1 k d = M1 #. grel1 k d .# unM1
-
-instance GRelative1 Par1 where
-  grel1 = coerce 
-
-instance Relative1 f => GRelative1 (Rec1 f) where
-  grel1 k d = Rec1 #. rel1 k d .# unRec1
-
---------------------------------------------------------------------------------
--- Relative2
---------------------------------------------------------------------------------
-
-class Relative2 f where
-  rel2 :: (Delta -> a -> a) -> (Delta -> b -> b) -> Delta -> f a b -> f a b
-  default rel2 :: Bifunctor f => (Delta -> a -> a) -> (Delta -> b -> b) -> Delta -> f a b -> f a b
-  rel2 f g d = bimap (f d) (g d) 
-
-frel2 :: Bifunctor f => (Delta -> a -> a) -> (Delta -> b -> b) -> Delta -> f a b -> f a b
-frel2 f g d = bimap (f d) (g d)
-
-instance Relative2 (,)
-instance Relative2 Either
