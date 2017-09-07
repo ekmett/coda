cd3eb5cc5e5e0cd6a216cd9123d1f79d0fd5a7ee
Edward Kmett <ekmett@gmail.com>
2017-09-07 08:47:08 +0100

    remove dead aeson bits

--- src/Coda/Util/Aeson.hs
+++ src/Coda/Util/Aeson.hs
@@ -17,13 +17,7 @@
 --------------------------------------------------------------------
 
 module Coda.Util.Aeson 
-  ( FromHook(..)
-  , ToHook(..)
-  , (?~), (?=)
-  , (??~)
-  , parseMissingAsNull
-  -- * pattern synonyms for the lens-aeson prisms
-  , pattern JSON
+  ( pattern JSON
   , pattern Value_
   , pattern Number_
   , pattern Double
@@ -39,57 +33,13 @@ import Coda.Util.Instances () -- Void instances
 import Control.Lens.Combinators
 import Control.Lens.Operators ((#))
 import Data.Aeson
-import Data.Aeson.Internal
-import Data.Aeson.Encoding.Internal hiding (Value)
 import Data.Aeson.Lens
-import Data.Aeson.Types
-import Data.HashMap.Strict as HashMap
-import Data.Maybe (fromMaybe)
 import Data.Scientific
 import Data.Text
 
 --------------------------------------------------------------------------------
 -- Utilities
 --------------------------------------------------------------------------------
-
-parseMissingAsNull :: FromJSON a => Object -> Text -> Parser a
-parseMissingAsNull m k = parseJSON (fromMaybe Null (HashMap.lookup k m)) <?> Key k
-
-infixr 8 ?=, !=, !~, ?~, ??~
-
-class Monoid (Ob v) => FromHook v where
-  type Ob v :: *
-  (!=) :: Text -> v -> Ob v
-
-instance x ~ Value => FromHook (Encoding' x) where
-  type Ob (Encoding' x) = Series
-  t != a       = pair t a
-
-instance FromHook Value where
-  type Ob Value = [(Text, Value)]
-  t != a       = [t .= a]
-
-class Monoid t => ToHook t where
-  (!~) :: ToJSON v => Text -> v -> t
-
-instance ToHook Series where
- t !~ a       = pair t (toEncoding a)
-
-instance x ~ (Text, Value) => ToHook [x] where
- t !~ a = [t .= toJSON a]
-
-(?=) :: FromHook v => Text -> Maybe v -> Ob v
-t ?= Just a  = t != a
-_ ?= Nothing = mempty
-
-(?~) :: (ToJSON v, ToHook t) => Text -> Maybe v -> t
-t ?~ Just a  = t !~ a
-_ ?~ Nothing = mempty
-
-(??~) :: (ToJSON a, ToHook r) => Text -> a -> r
-t ??~ a = case toJSON a of
-  Null -> mempty
-  x    -> t !~ x
 
 pattern JSON :: (FromJSON a, ToJSON a, AsJSON t) => () => a -> t
 pattern JSON a <- (preview _JSON -> Just a) where
