24d4b0c84efa98e958da0fba374f133889bc5c7d
Edward Kmett <ekmett@gmail.com>
2017-10-11 13:00:37 -0400

    finish changeChange/composeChange. introduce 'generalize', an idempotent monad on Change

--- ref/Change.hs
+++ ref/Change.hs
@@ -89,8 +89,8 @@ instance Num Grade where
 invGrade :: Grade -> Grade
 invGrade (Grade a b) = Grade b a
 
-idGrade :: Delta -> Grade 
-idGrade d = Grade d d 
+idGrade :: Delta -> Grade
+idGrade d = Grade d d
 
 composeGrade :: Alternative f => Grade -> Grade -> f Grade
 composeGrade (Grade b' c) (Grade a b) = Grade a c <$ guard (b == b')
@@ -115,7 +115,7 @@ instance Measured Edit where
 
 -- @delta = delta . measure@
 instance HasDelta Edit where
-  delta (Edit n f _) = n + delta f 
+  delta (Edit n f _) = n + delta f
 
 instance HasOrderedDelta Edit
 
@@ -147,7 +147,7 @@ editDelta (Edit n _ _) i
   | i < 0 = die "negative index"
   | i < n = pure i
   | otherwise = die "index too large"
-  
+
 stripSuffixes :: FingerTree Text -> Text -> Partial Text
 stripSuffixes (xs :> x) y = case stripSuffix x y of
   Just y' -> stripSuffixes xs y'
@@ -171,35 +171,49 @@ editText (Edit n f t) s = do
 --------------------------------------------------------------------------------
 
 -- |
--- Invariants: 
+-- Invariants:
 --
 -- 1) no two edits with 0 spaces between them. they get coalesced into a single edit node
 -- 2) all edits have one of the finger-trees non-empty
-data Change
-  = Change !(FingerTree Edit) !Delta
+data Change = Change !(FingerTree Edit) !Delta
   deriving (Eq,Ord,Show)
+  -- = Change !(FingerTree Edit) !Delta
+
+#if __GLASGOW_HASKELL__ >= 802
+{-# complete_patterns (C0,CN)|Change #-}
+#endif
+
+changePattern :: Change -> (FingerTree Edit, Delta)
+changePattern (C0 d)      = (mempty, d)
+changePattern (CN e es d) = (e :< es, d)
+
+pattern C0 :: Delta -> Change
+pattern C0 d = Change EmptyTree d
+
+pattern CN :: Edit -> FingerTree Edit -> Delta -> Change
+pattern CN x xs d = Change (x :< xs) d
 
 --instance Show Change where
 --  show e = case ppChange e of (x,y,z) -> x ++ "\n" ++ y ++ "\n" ++ z
 
 instance Relative Change where
-  rel d (Change (e :< es) d') = Change (rel d e :< es) d'
-  rel d (Change EmptyTree d') = Change EmptyTree (d+d')
+  rel d (C0 d')      = C0 (d+d')
+  rel d (CN e es d') = CN (rel d e) es d'
 
 -- | This measures the size of the domain, @delta (invChange d)@ measures the codomain
 instance HasDelta Change where
-  delta (Change xs d) = delta (measure xs) + d
+  delta (Change es d) = delta es + d
 
 instance Measured Change where
   type Measure Change = Grade
-  measure (Change xs d) = rel d (measure xs) -- abuses commutativity
+  measure (Change es d) = measure es + Grade d d
 
 invChange :: Change -> Change
-invChange (Change xs d) = Change (fmap' invEdit xs) d
+invChange (Change es d) = Change (fmap' invEdit es) d
 
 concatEdits :: FingerTree Edit -> Delta -> FingerTree Edit -> Delta -> Change
 concatEdits EmptyTree 0 ys e = Change ys e
-concatEdits EmptyTree d EmptyTree e = Change EmptyTree (d+e)
+concatEdits EmptyTree d EmptyTree e = C0 (d+e)
 concatEdits xs d EmptyTree e = Change xs (d+e)
 concatEdits EmptyTree d (t :< ys) e = Change (rel d t <| ys) e
 concatEdits (xs :> Edit n as bs) 0 (Edit 0 cs ds :< ys) e = Change ((xs |> Edit n (as <> cs) (bs <> ds)) <> ys) e
@@ -208,14 +222,14 @@ concatEdits xs d (t :< ys) e = Change ((xs |> rel d t) <> ys) e
 -- | given a change x that will successfully apply to t, and a change y that successfully applies to s
 -- concatChange x y successfully applies to (t <> s)
 instance Semigroup Change where
-  Change EmptyTree 0 <> rhs = rhs
-  lhs <> Change EmptyTree 0 = lhs
+  C0 0 <> rhs = rhs
+  lhs <> C0 0 = lhs
   Change xs d <> Change ys e = concatEdits xs d ys e
-  
+
 instance Monoid Change where
-  mempty = Change EmptyTree 0
+  mempty = C0 0
   mappend = (<>)
-    
+
 newtype App f a = App { runApp :: f a } deriving (Functor,Applicative)
 
 instance (Applicative f, Monoid a) => Monoid (App f a) where
@@ -223,7 +237,7 @@ instance (Applicative f, Monoid a) => Monoid (App f a) where
   mappend = liftA2 mappend
 
 -- | /O(log(min(k,n-k)))/ where there are @n@ edits, @k@ of which occur before the position in question
--- 
+--
 -- @
 -- censor (changeDelta e >=> changeDelta (invChange e) >=> changeDelta e) = censor (changeDelta e)
 -- censor (changeDelta (invChange e) >=> changeDelta e >=> changeDelta (invChange e)) = censor (changeDelta (invChange e))
@@ -236,7 +250,7 @@ changeDelta (Change xs d) i = case search (\m _ -> i < delta m) xs of
     | otherwise -> die "changePos: Past end"
   OnLeft -> die "changePos: index < 0"
   Nowhere -> die "changePos: Nowhere"
-  
+
 changeText :: Change -> Text -> Partial Text
 changeText c@(Change xs d) t
   | o <- delta xs, delta t == o + d = (<> dropDelta o t) <$> runApp (foldMapWithPos step xs)
@@ -247,8 +261,8 @@ class FromEdit a where
   edit :: Edit -> a
 
 instance FromEdit Change where
-  edit e 
-    | Grade 0 0 <- measure e = Change mempty 0
+  edit e
+    | Grade 0 0 <- measure e = C0 0
     | otherwise = Change (FingerTree.singleton e) 0
 
 instance FromEdit Edit where
@@ -293,7 +307,7 @@ pad n = Prelude.replicate (units n) ' '
 
 instance Pretty Edit where
   pp (Edit b f t)
-    | d <- delta f, e <- delta t, c <- max e d 
+    | d <- delta f, e <- delta t, c <- max e d
     = pp b <> ppBar <> ( foldMap unpack f <> pad (c - d), pad c, foldMap unpack t <> pad (c - e))
 
 instance Pretty Change where
@@ -307,41 +321,62 @@ pretty e = traverseOf_ each putStrLn (pp e)
 --------------------------------------------------------------------------------
 
 -- |
--- the middle result is an under-determined 'del' that can be slid freely between the two changes
---
--- @c = case splitChange d c of (l, m, r) -> l <> dels m <> r@
-splitChange :: Delta -> Change -> (Change, FingerTree Text, Change)
+-- @c = case splitChange d c of (l, r) -> l <> r
+-- grade c = case splitChange d c of (l, r) -> grade l + grade r
+-- delta (fst $ splitChange d c) = max 0 (min d (grade c))
+-- delta (snd $ splitChange d c) = max 0 (min d (grade c - d))
+-- @
+splitChange :: Delta -> Change -> (Change, Change)
 splitChange i c@(Change xs d) = case search (\m _ -> i <= delta m) xs of
   Nowhere -> error "splitChange: Nowhere"
-  OnLeft -> (mempty, mempty, c)
-  OnRight | i' <- i - delta xs -> (Change xs i', mempty, cpy (d-i))
-  Position l (Edit n f t) r 
-    | j < n -> (Change l j, mempty, Change (Edit (n-j) f t <| r) d)
+  OnLeft -> (mempty, c)
+  OnRight | i' <- i - delta xs -> (Change xs i', cpy (d-i))
+  Position l (Edit n f t) r
+    | j < n -> (Change l j, Change (Edit (n-j) f t <| r) d)
     | otherwise -> case search (\m _ -> j <= delta m) f of
       Nowhere -> error "splitChange: Nowhere(2)"
-      OnLeft -> (Change l n, t, Change (Edit 0 f mempty <| r) d) -- j == n
-      OnRight -> error "splitChange: OnRight(2)"
+      OnLeft -> (Change l n <> dels t, inss f <> Change r d)
+      OnRight -> (Change (l |> Edit n f t) 0, Change r d)
       Position fl (splitDelta (j - delta fl) -> (fml, fmr)) fr ->
-        (Change (l :> Edit n (fl |> fml) mempty) 0, t, Change (Edit 0 (fmr <| fr) mempty :< r) d) 
+        (Change (l :> Edit n (fl |> fml) t) 0, Change (Edit 0 (fmr <| fr) mempty :< r) d)
     where j = i - delta l
 
-splitChange_ :: Delta -> Change -> (Change, Change)
-splitChange_ i c = case splitChange i c of
-  (l,md,r) -> (l <> dels md, r) 
- 
 -- | @
 -- censor (editChange e >=> editChange (invEdit e) >=> editChange e) = censor (editChange e)
 -- censor (editChange (invEdit e) >=> editChange e >=> editChange (invEdit e)) = censor (editChange (invEdit e))
 -- @
 editChange :: Edit -> Change -> Partial Change
-editChange (Edit d f t) c = case splitChange_ d c of
-  (l,r) -> do
-    t' <- changeText r (fold t)
-    pure $ l <> dels f <> ins t'
+editChange (Edit d f t) c = do
+  let (l,r) = splitChange d c
+  t' <- changeText r (fold t)
+  pure $ l <> dels f <> ins t'
 
--- changeChange f g   represents categorical composition: g . f 
+-- @changeChange f g@ represents categorical composition: @g . f@
 changeChange :: Change -> Change -> Partial Change
 changeChange (Change xs0 d0) c0 = go xs0 d0 c0 where
-  go (e :< es) d c = case splitChange_ (delta e) c of (l, r) -> (<>) <$> editChange e l <*> go es d c
-  go EmptyTree d c 
-    | Grade o n <- measure c = c <$ unless (n == d) (die $ "changeChange: mismatch " ++ show (o,n,d))
+  go (e :< es) d c = do
+    let (l, r) = splitChange (delta (invEdit e)) c
+    (<>) <$> editChange e l <*> go es d r
+  go EmptyTree d c = do
+    unless (delta c == d) $ die $ "changeChange: mismatch " ++ show (delta c,d)
+    pure c
+
+-- | @composeChange f g@ represents categorical composition @f . g@
+composeChange :: Change -> Change -> Partial Change
+composeChange = flip changeChange
+
+dropDeltas :: Delta -> FingerTree Text -> FingerTree Text
+dropDeltas i xs = case search (\m _ -> i <= delta m) xs of
+  Position l e r -> dropDelta (i - delta l) e <| r
+  OnLeft -> xs
+  OnRight -> mempty
+  Nowhere -> error "dropDeltas: Nowhere"
+
+-- | build a strictly more general function that produces the same answer on all accepted inputs.
+--
+-- An idempotent monad on Change
+generalize :: Change -> Change
+generalize (Change xs d) = foldMap go xs <> cpy d where
+  go (Edit n f t)
+    | k <- min (delta f) (delta t)
+    = cpy (n + k) <> inss (dropDeltas k f) <> dels (dropDeltas k t)
