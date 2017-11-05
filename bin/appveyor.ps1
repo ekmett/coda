33badd338d90abdaf923c604825c507a642f9ab9
Edward Kmett <ekmett@gmail.com>
2017-11-05 03:07:44 -0500

    check exit codes on appveyor

--- bin/appveyor.ps1
+++ bin/appveyor.ps1
@@ -16,15 +16,21 @@ if ($phase -eq "install") {
   }
   "> unzip stack"
   7z x -y stack.zip stack.exe
+  if ($LASTEXITCODE -ne 0) {exit 1}
   Move-Item -Path stack.exe -Destination bin\stack.exe
+  if ($LASTEXITCODE -ne 0) {exit 1}
 } elseif($phase -eq "build_script") {
   "> stack setup"
   cmd /c 'bin\stack setup 2>&1 1>&2 > nul'
+  if ($LASTEXITCODE -ne 0) {exit 1}
   "> stack path"
   cmd /c 'bin\stack path 2>&1'
+  if ($LASTEXITCODE -ne 0) {exit 1}
   "> stack exec env"
   cmd /c 'bin\stack exec env 2>&1'
+  if ($LASTEXITCODE -ne 0) {exit 1}
   $env:APPVEYOR_SAVE_CACHE_ON_ERROR = "true"
   "> stack build"
   cmd /c 'echo | bin\stack --no-terminal build --ghc-options=-rtsopts 2>&1'
+  if ($LASTEXITCODE -ne 0) {exit 1}
 }
