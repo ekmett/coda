3fc8ae10998e0f5ce931c87fce073d32667c358b
Edward Kmett <ekmett@gmail.com>
2017-11-05 02:47:50 -0500

    don't echo PATH!

--- bin/coda.sh
+++ bin/coda.sh
@@ -5,8 +5,6 @@ BIN_DIR=${0%/*}
 ABS_BIN_DIR=`cd "$BIN_DIR"; pwd`
 BASE_DIR=${ABS_BIN_DIR%/*}
 export PATH="$PATH:$BASE_DIR/dist-newstyle/build/x86_64-osx/ghc-8.2.1/coda-0.0.1/c/coda/build/coda"
-echo $PATH
-
 export CODA_SERVER_PATH=`which coda`
 
 if [ "X" = "X$CODA_SERVER_PATH" ]; then
