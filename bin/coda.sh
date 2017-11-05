52b73466ece25d5537539177a227652448774eb8
Edward Kmett <ekmett@gmail.com>
2017-11-05 01:04:11 -0500

    extend the path with the appropriate dist-newstyle directory

--- bin/coda.sh
+++ bin/coda.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+# temporary hack to extend the path to find it in a dist-newstyle folder on my machine without install
+BIN_DIR=${0%/*}
+ABS_BIN_DIR=`cd "$BIN_DIR"; pwd`
+BASE_DIR=${ABS_BIN_DIR%/*}
+export PATH="$PATH:$BASE_DIR/dist-newstyle/build/x86_64-osx/ghc-8.2.1/coda-0.0.1/c/coda/build/coda"
+echo $PATH
+
 export CODA_SERVER_PATH=`which coda`
 
 if [ "X" = "X$CODA_SERVER_PATH" ]; then
