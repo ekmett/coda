e047da25208a98cc031ecf1a3285edc026a23377
Edward Kmett <ekmett@gmail.com>
2017-11-26 14:12:34 -0500

    optimize clause -> bdd construction

--- lib/bdd/Ersatz/Solver/BDD.hs
+++ lib/bdd/Ersatz/Solver/BDD.hs
@@ -1,5 +1,6 @@
 {-# language ScopedTypeVariables #-}
 {-# language FlexibleContexts #-}
+{-# language BangPatterns #-}
 module Ersatz.Solver.BDD where
 
 import Control.Applicative
@@ -10,15 +11,38 @@ import Data.Proxy
 import Ersatz.Problem
 import Ersatz.Solution
 
+literal :: Cached s => Int -> BDD s
+literal 1    = One
+literal (-1) = Zero
+literal i    = polarize i $ var (abs i)
+
+-- use a more deliberate bdd construction for the clauses that constructs from the bottom up
+clause :: Cached s => IntSet -> BDD s
+-- clause = IntSet.foldr (\a r -> literal a `BDD.or` r) Zero
+clause is = case splitMember 1 is of
+  (l,t,r)
+     | t -> One -- True is a member of the clause
+     | otherwise -> start Zero (IntSet.toAscList $ fst $ IntSet.split (-1) l) (IntSet.toDescList r)
+  where
+    start acc [] ys = posi acc ys
+    start acc xs [] = negi acc xs
+    start acc (x:xs) (y:ys) = go acc (negate x) xs y ys
+    posi !acc [] = acc
+    posi acc (y:ys) = posi (BDD y acc One) ys
+    negi !acc [] = acc
+    negi acc (x:xs) = negi (BDD (negate x) One acc) xs
+    go !acc x xs y ys = case compare x y of
+      GT | acc' <- BDD x One acc -> case xs of
+        [] -> posi (BDD y acc' One) ys
+        x':xs' -> go acc' (negate x') xs' y ys
+      EQ -> One -- x \/ ~x => this clause is always true
+      LT | acc' <- BDD y acc One -> case ys of
+        [] -> negi (BDD x One acc') xs
+        y':ys' -> go acc' x xs y' ys'
+
 robdd :: Monad m => Solver SAT m
 robdd problem = pure $ reifyCache $ \(Proxy :: Proxy s) ->
-  let literal 1    = One
-      literal (-1) = Zero
-      literal i    = polarize i $ var (abs i)
-
-      clause = IntSet.foldr (\a r -> literal a `BDD.or` r) Zero
-
-      solve = Prelude.foldr (\a r -> clause a `BDD.and` r) One
+  let solve = Prelude.foldr (\a r -> clause a `BDD.and` r) One
 
       result :: BDD s
       result = solve (dimacsClauses problem)
