3f29f1b5cc67eba7fd2682547981a3a77ef8dab3
Edward Kmett <ekmett@gmail.com>
2018-04-23 13:06:54 -0400

    replace zipWithT with a more tailored combinator

--- src/Coda/LLVM.hs
+++ src/Coda/LLVM.hs
@@ -1,11 +1,14 @@
-module Coda.LLVM (fun) where
+module Coda.LLVM
+  ( fun
+  ) where
 
 import Data.Foldable (toList)
 import Data.Traversable (mapAccumL)
-import LLVM.AST as AST
+import LLVM.AST
 import LLVM.IRBuilder as IR
 
--- | A slightly safer version of 'IR.function' using the encoding trick from the @ad@ package
+-- | A slightly safer version of 'IR.function' using the encoding trick
+-- from the @ad@ package
 fun
   :: (MonadModuleBuilder m, Traversable f)
   => Name  -- ^ Function name
@@ -13,7 +16,8 @@ fun
   -> Type  -- ^ Return type
   -> (f Operand -> IRBuilderT m ())  -- ^ Function body builder
   -> m Operand
-fun n as r f = IR.function n (toList as) r $ \xs -> f (zipWithT const xs as)
+fun n as r f = IR.function n (toList as) r $ \xs -> f (refill xs as)
 
-zipWithT :: (Foldable f, Traversable g) => (a -> b -> c) -> f a -> g b -> g c
-zipWithT f as = snd . mapAccumL (\(a:as') b -> (as', f a b)) (toList as)
+-- | assumes list length and container length match
+refill :: Traversable f => [a] -> f b -> f a
+refill = fmap snd . mapAccumL (\(a:as') _ -> (as', a))
