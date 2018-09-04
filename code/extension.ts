e1f734a80916d61bf29278db710722e60d23effc
Edward Kmett <ekmett@gmail.com>
2018-09-04 01:55:26 -0700

    tweak extension to use new executable name

--- code/extension.ts
+++ code/extension.ts
@@ -3,7 +3,7 @@ import * as path from 'path';
 import { workspace, Disposable, ExtensionContext } from 'vscode';
 import { LanguageClient, LanguageClientOptions, SettingMonitor, ServerOptions } from 'vscode-languageclient';
 export function activate(context: ExtensionContext) {
-  let serverPath = context.asAbsolutePath(path.join('bin', process.platform === 'win32' ? 'coda.bat' : 'coda.sh'));
+  let serverPath = context.asAbsolutePath(path.join('bin', process.platform === 'win32' ? 'codex.bat' : 'codex.sh'));
   let serverOptions: ServerOptions = {
     run : { command: serverPath, args: ['server'] },
     debug: { command: serverPath, args: ['server', '--debug'] }
